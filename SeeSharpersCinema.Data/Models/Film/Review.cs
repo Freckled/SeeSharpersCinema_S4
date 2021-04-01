6e33eefb5c056b8dc2a3b4c332ace2aeb7fbcd4c
Noel Bissels <mail@freckled.nl>
2021-04-02 00:16:16 +0200

    rating/review start

---
+++ SeeSharpersCinema.Data/Models/Film/Review.cs
@@ -0,0 +1,17 @@
+﻿using SeeSharpersCinema.Models.Film;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeeSharpersCinema.Data.Models.Film
+{
+    public class Review
+    {
+        public long Id { get; set; }
+        public int score { get; set; }
+        public Movie Movie { get; set; }
+        public string Text { get; set; }
+    }
+}
